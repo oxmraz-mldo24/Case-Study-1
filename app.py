a15bc9b999b9d00fa962a16b48954247581464bc
Julian Hans <julian.hans@student.uni-luebeck.de>
2024-09-14 19:54:27 -0400

    removed force pushed, load models separately to optimize ram usage

--- app.py
+++ app.py
@@ -1,7 +1,10 @@
 # external imports
+import gc
+import logging as log
 import time
 import uuid
 import gradio as gr
+import os
 
 # local imports
 from blip_image_caption_large import Blip_Image_Caption_Large
@@ -9,12 +12,11 @@ from phi3_mini_4k_instruct import Phi3_Mini_4k_Instruct
 from musicgen_small import Musicgen_Small
 import config
 
+log.basicConfig(level=log.INFO)
+
+
 class Image_To_Music:
     def __init__(self):
-        self.image_caption_model = Blip_Image_Caption_Large()
-        self.text_generation_model = Phi3_Mini_4k_Instruct()
-        self.music_generation_model = Musicgen_Small()
-
         self.image_path = None
         self.generated_caption = None
         self.generated_description = None
@@ -23,28 +25,71 @@ class Image_To_Music:
         self.caption_generation_duration = -1
         self.description_generation_duration = -1
         self.music_generation_duration = -1
-    
+        self.create_output_folder()
+
+
+    # ----ATTRIBUTION-START----
+    # LLM: Github Copilot
+    # PROMPT: create an output folder for the generated audio files
+    # EDITS: /
+    def create_output_folder(self):
+        os.makedirs(config.AUDIO_DIR, exist_ok=True)
+    # -----ATTRIBUTION-END-----
+
     def caption_image(self, image_path):
+        log.info("Captioning Image...")
         caption_start_time = time.time()
+
+        # load model
+        self.image_caption_model = Blip_Image_Caption_Large()
+
         self.image_path = image_path
         self.generated_caption = self.image_caption_model.caption_image_local_pipeline(self.image_path)[0]["generated_text"]
+
+        # delete model to free up ram
+        del self.image_caption_model
+        gc.collect()
+
         self.caption_generation_duration = time.time() - caption_start_time
+        log.info(f"Captioning Complete in {self.caption_generation_duration:.2f} seconds: {self.generated_caption}")
         return self.generated_caption
     
     def generate_description(self):
+        log.info("Generating Music Description...")
         description_start_time = time.time()
+
+        # load model
+        self.text_generation_model = Phi3_Mini_4k_Instruct()
+
         messages = [
             {"role": "system", "content": "You are an image caption to song description converter with a deep understanding of Music and Art. You are given the caption of an image. Your task is to generate a textual description of a musical piece that fits the caption. The description should be detailed and vivid, and should include the genre, mood, instruments, tempo, and other relevant information about the music. You should also use your knowledge of art and visual aesthetics to create a musical piece that complements the image. Only output the description of the music, without any explanation or introduction. Be concise."},
             {"role": "user", "content": self.generated_caption},
         ]
         self.generated_description = self.text_generation_model.generate_text_local_pipeline(messages)[-1]['generated_text'][-1]['content']
+
+        # delete model to free up ram
+        del self.text_generation_model
+        gc.collect()
+
         self.description_generation_duration = time.time() - description_start_time
+        log.info(f"Description Generation Complete in {self.description_generation_duration:.2f} seconds: {self.generated_description}")
         return self.generated_description
     
     def generate_music(self):
+        log.info("Generating Music...")
         music_start_time = time.time()
+        
+        # load model
+        self.music_generation_model = Musicgen_Small()
+
         self.music_generation_model.generate_music_local_pipeline(self.generated_description, self.audio_path)
+        
+        # delete model to free up ram
+        del self.music_generation_model
+        gc.collect()
+
         self.music_generation_duration = time.time() - music_start_time
+        log.info(f"Music Generation Complete in {self.music_generation_duration:.2f} seconds: {self.audio_path}")
         return self.audio_path
     
     def get_durations(self):
