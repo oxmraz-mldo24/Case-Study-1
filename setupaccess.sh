52d92f0d5ad73d6146fdcb414f8ad33079116c21
oxmraz-mldo24 <179968616+oxmraz-mldo24@users.noreply.github.com>
2024-09-26 15:54:22 -0400

    Create setupaccess.yml

--- setupaccess.sh
+++ setupaccess.sh
@@ -17,11 +17,29 @@ echo "setupaccess.sh: remove known host keys for the server to avoid the REMOTE 
 cat group17.pub > authorized_keys
 echo "setupaccess.sh: make an authorized_keys file with group17.pub as an authorized key"
 
-scp -o StrictHostKeyChecking=no -i group17 -P 22017 authorized_keys student-admin@paffenroth-23.dyn.wpi.edu:/home/student-admin/.ssh
-echo "setupaccess.sh: copy authorized_keys file to server"
+rm group17.pub
+echo "setupaccess.sh: remove group17.pub file from host"
+
+scpOutput=$(scp -o StrictHostKeyChecking=no -i group17 -P 22017 authorized_keys student-admin@paffenroth-23.dyn.wpi.edu:/home/student-admin/.ssh 2>&1)
+echo "setupaccess.sh: try to copy authorized_keys file to server"
+
+if [[ "$scpOutput" = *"student-admin@paffenroth-23.dyn.wpi.edu: Permission denied (publickey)."* ]];
+then touch student-admin_key
+    echo "$STUDENT_ADMIN_KEY" > student-admin_key
+    echo "setupaccess.sh: make student-admin_key file"
+    scp -o StrictHostKeyChecking=no -i student-admin_key -P 22017 authorized_keys student-admin@paffenroth-23.dyn.wpi.edu:/home/student-admin/.ssh
+    echo "setupaccess.sh: copied authorized_keys file to server with student-admin_key"
+    rm student-admin_key
+    echo "setupaccess.sh: remove student-admin_key from host"
+else
+    echo "setupaccess.sh: copied authorized_keys file to server with our private key"
+fi
 
 rm authorized_keys
 echo "setupaccess.sh: remove authorized_keys file from host"
 
 ssh -p 22017 -i group17 -o StrictHostKeyChecking=no student-admin@paffenroth-23.dyn.wpi.edu
 echo "setupaccess.sh: try to ssh in"
+
+rm group17
+echo "setupaccess.sh: remove group17 file from host"
